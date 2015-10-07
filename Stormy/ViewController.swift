d7d8390861c4a182606470edc40ffe6da199ca59
Sachin Virdy <svirdy1@gmail.com>
2015-10-06 23:30:06 -0700

    update nav bar style and header layout

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -10,75 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var currentTemperatureLabel: UILabel?
-    @IBOutlet weak var currentHumidityLabel: UILabel?
-    @IBOutlet weak var currentPrecipitationLabel: UILabel?
-    @IBOutlet weak var currentWeatherIcon: UIImageView?
-    @IBOutlet weak var currentWeatherSummary: UILabel?
-    @IBOutlet weak var refreshButton: UIButton?
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView?
-    
-    
-    private let forecastAPIKey = "e3c8c8c6681cb3eb7e4417e08e115037"
-    let coordinate: (lat: Double, long: Double) = (37.8267,-122.423)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        retrieveWeatherForecast()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func retrieveWeatherForecast() {
-        let forecastService = ForecastService(APIKey: forecastAPIKey)
-        forecastService.getForecast(coordinate.lat, long: coordinate.long) {
-            (let currently) in
-            if let currentWeather = currently {
-                dispatch_async(dispatch_get_main_queue()) {
-                    // Execute closure
-                    
-                    if let temperature = currentWeather.temperature {
-                        self.currentTemperatureLabel?.text = "\(temperature)º"
-                    }
-                    
-                    if let humidity = currentWeather.humidity {
-                        self.currentHumidityLabel?.text = "\(humidity)%"
-                    }
-                    
-                    if let precipitation = currentWeather.precipProbability {
-                        self.currentPrecipitationLabel?.text = "\(precipitation)%"
-                    }
-                    
-                    if let icon = currentWeather.icon {
-                        self.currentWeatherIcon?.image = icon
-                    }
-                    
-                    if let summary = currentWeather.summary {
-                        self.currentWeatherSummary?.text = summary
-                    }
-                    
-                    self.toggleRefreshAnimation(false)
-                }
-            }
-        }
-    }
     
-    @IBAction func refreshWeather() {
-        toggleRefreshAnimation(true)
-        retrieveWeatherForecast()
-    }
-    
-    func toggleRefreshAnimation(on: Bool) {
-        refreshButton?.hidden = on
-        if on {
-            activityIndicator?.startAnimating()
-        } else {
-            activityIndicator?.stopAnimating()
-        }
-    }
 }
 
--- Stormy/WeeklyTableViewController.swift
+++ Stormy/WeeklyTableViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
+
 class WeeklyTableViewController: UITableViewController {
 
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentTemperatureRangeLabel: UILabel!
+    
+    let forecastAPIKey = "e3c8c8c6681cb3eb7e4417e08e115037"
+    let coordinate: (lat: Double, long: Double) = (37.8267,-122.423)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.backgroundView = BackgroundView()
+        configureView()
+       
+        retrieveWeatherForecast()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func configureView() {
+        
+        // Set table view's background propery
+        tableView.backgroundView = BackgroundView()
+        
+        // Change the font and size of nav bar text
+        if let navBarFont = UIFont(name: "HelveticaNeue-Thin", size: 20.0) {
+        let navBarAttributesDictionary: [String:AnyObject]? = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: navBarFont]
+            navigationController?.navigationBar.titleTextAttributes = navBarAttributesDictionary
+        }
     }
 
     // MARK: - Table view data source
@@ -32,5 +54,38 @@ class WeeklyTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
+
+
+
+
+// MARK: - Weather Fetching
+
+    func retrieveWeatherForecast() {
+        let forecastService = ForecastService(APIKey: forecastAPIKey)
+        forecastService.getForecast(coordinate.lat, long: coordinate.long) {
+            (let currently) in
+            if let currentWeather = currently {
+                dispatch_async(dispatch_get_main_queue()) {
+                    // Execute closure
+                    
+                    if let temperature = currentWeather.temperature {
+                        self.currentTemperatureLabel?.text = "\(temperature)º"
+                    }
+                    
+                    if let precipitation = currentWeather.precipProbability {
+                        self.currentPrecipitationLabel?.text = "Rain: \(precipitation)%"
+                    }
+                    
+                    if let icon = currentWeather.icon {
+                        self.currentWeatherIcon?.image = icon
+                    }
+                    
+                    
+                    
+                }
+            }
+        }
+    }
+
 
 }
