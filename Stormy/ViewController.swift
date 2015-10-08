dbf582d7f6a2601f05882af9a0772deb2ee4f380
Sachin Virdy <svirdy1@gmail.com>
2015-10-08 16:49:17 -0700

    add detail view segue and background

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var dailyWeather: DailyWeather? {
+        didSet {
+            configureView()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func configureView() {
+        if let weather = dailyWeather {
+            self.title = weather.day
+        }
+        
+        // Configure navbar back button
+        if let buttonFont = UIFont(name: "HelveticaNeue-Thin", size: 20.0) {
+            let barButtonAttributesDictionary: [String:AnyObject]? = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: buttonFont
+            ]
+            UIBarButtonItem.appearance().setTitleTextAttributes(barButtonAttributesDictionary, forState: .Normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
