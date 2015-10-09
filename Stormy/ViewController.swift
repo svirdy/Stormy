20d6a34ad730a9b4c7955d380ad99a41afb31eb5
Sachin Virdy <svirdy1@gmail.com>
2015-10-08 19:22:23 -0700

    finished app

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -16,13 +16,40 @@ class ViewController: UIViewController {
         }
     }
 
+    @IBOutlet weak var weatherIcon: UIImageView?
+    @IBOutlet weak var summaryLabel: UILabel?
+    @IBOutlet weak var sunriseTimeLabel: UILabel?
+    @IBOutlet weak var sunsetTimeLabel: UILabel?
+    @IBOutlet weak var lowTemperatureLabel: UILabel?
+    @IBOutlet weak var highTemperatureLabel: UILabel?
+    @IBOutlet weak var precipitationLabel: UILabel?
+    @IBOutlet weak var humidityLabel: UILabel?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureView()
     }
     
     func configureView() {
         if let weather = dailyWeather {
+            // Update UI with information from the data model
+            weatherIcon?.image = weather.largeIcon
+            summaryLabel?.text = weather.summary
+            sunriseTimeLabel?.text = weather.sunriseTime
+            sunsetTimeLabel?.text = weather.sunsetTime
+            
+            if let lowTemp = weather.minTemperature,
+            let highTemp = weather.maxTemperature,
+            let rain = weather.precipChance,
+                let humidity = weather.humidity {
+                    lowTemperatureLabel?.text = "\(lowTemp)º"
+                    highTemperatureLabel?.text = "\(highTemp)º"
+                    precipitationLabel?.text = "\(rain)%"
+                    humidityLabel?.text = "\(humidity)%"
+            }
+            
             self.title = weather.day
         }
         
@@ -32,6 +59,7 @@ class ViewController: UIViewController {
             ]
             UIBarButtonItem.appearance().setTitleTextAttributes(barButtonAttributesDictionary, forState: .Normal)
         }
+        
     }
 
     override func didReceiveMemoryWarning() {
